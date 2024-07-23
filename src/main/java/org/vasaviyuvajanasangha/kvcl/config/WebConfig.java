ff985dcc98a301330cbaf0aa3772c5562ab7e029
Suchith Kumar <suchith.1995.kumar@gmail.com>
2024-07-23 06:14:05 +0530

    1.0.4 template mistakes corrected

--- src/main/java/org/vasaviyuvajanasangha/kvcl/config/WebConfig.java
+++ src/main/java/org/vasaviyuvajanasangha/kvcl/config/WebConfig.java
@@ -11,6 +11,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.DelegatingWebMvcConfiguration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.vasaviyuvajanasangha.kvcl.handlers.LogoutSuccessHandler;
 import org.vasaviyuvajanasangha.kvcl.handlers.SuccessHandler;
 import org.vasaviyuvajanasangha.kvcl.service.MyUserDetailsManager;
@@ -44,7 +46,7 @@ public class WebConfig {
 //                		.dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
 				.requestMatchers(PathRequest.toStaticResources().atCommonLocations())
 				.permitAll()
-				.requestMatchers("/images/**", "/css/**", "/js/**", "/WEB-INF/jsp/**", "/favicon.ico", "/login","static/**")
+				.requestMatchers("/images/**", "/css/**", "/js/**", "/webjars/**", "/favicon.ico", "/login","static/**","/error")
 				.permitAll()
 				.requestMatchers("/register-user", "/", "/welcome").permitAll()
 				.requestMatchers("/admin-home").hasAnyRole("ADMIN").requestMatchers("/user-home")
@@ -52,9 +54,7 @@ public class WebConfig {
 				.formLogin(form -> form.loginPage("/login").successHandler(new SuccessHandler()).permitAll())
 				.logout(logout -> logout.logoutUrl("/logout").logoutSuccessHandler(new LogoutSuccessHandler())
 						.clearAuthentication(true));
-//				/error?continue HTTP/1.1" 500
-//		2024-07-21 15:21:01 default[20240721t185207]  "GET /favicon.ico HTTP/1.1" 404
-//		2024-07-21 15:21:06 default[20240721t185207]  "GET /error?continue HTTP/1.1" 500
+
 		return http.build();
 	}
 
